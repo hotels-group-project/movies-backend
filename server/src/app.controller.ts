aea8b1af013b71773771a62656c3b470f1ce6b55
Dzianis Kundaliou <dark_dionis@mail.ru>
2023-04-17 11:06:15 +0300

    microservice initialisation, rabbitmq set up between microservice and server

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('FILM_SERVICE') private readonly client: ClientProxy) {}
 
-  @Get('/hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('')
+  getHello(): Observable<string> {
+    return this.client.send('sayhello', '');
   }
 }
