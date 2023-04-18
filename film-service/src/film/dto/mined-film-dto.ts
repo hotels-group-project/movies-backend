3b06f4b512711eb18ee2e86b020b5a9946632d65
Dzianis Kundaliou <dark_dionis@mail.ru>
2023-04-18 15:40:16 +0300

    fix: updated .gitignore and deleted unnecessary dto's

--- film-service/src/film/dto/mined-film-dto.ts
+++
@@ -1,8 +0,0 @@
-export class MinedFilmDto{            
-    name : string;        
-    year : number;    
-    description : string    
-    movieLength: number;    
-    ageRating: number;    
-    countries: string;        
-}
